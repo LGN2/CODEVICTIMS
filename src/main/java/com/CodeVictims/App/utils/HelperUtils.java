6af36b4ba472b9884e61e34492e309320894141f
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-30 14:31:44 +0400

    feat (utils): HelperUtils added a getFullName util

--- src/main/java/com/CodeVictims/App/utils/HelperUtils.java
+++ src/main/java/com/CodeVictims/App/utils/HelperUtils.java
@@ -1,4 +1,16 @@
 package com.CodeVictims.App.utils;
 
 public class HelperUtils {
+    public static String getFullName(String firstName, String lastName){
+        if (firstName == null && lastName == null){
+            return "";
+        }
+        if (firstName == null){
+            return lastName.trim();
+        }
+        if (lastName == null){
+            return firstName.trim();
+        }
+        return firstName.trim() +" "+ lastName.trim();
+    }
 }
