dc0f8d51822a6da5e19e3ff4a6f20a86731b7de6
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-30 12:07:57 +0400

    feat(requestobjects): CustomerRequest added getter, setter and attributes

--- src/main/java/com/CodeVictims/App/requestobjects/CustomerRequest.java
+++ src/main/java/com/CodeVictims/App/requestobjects/CustomerRequest.java
@@ -1,4 +1,12 @@
 package com.CodeVictims.App.requestobjects;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CustomerRequest {
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerEmail;
 }
