9c0d88652a8e4ad2d0bd8aa0a8d8de823ab87a4b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-30 16:38:33 +0400

    feat (controllers): CustomerController finished the customer controller

--- src/main/java/com/CodeVictims/App/controllers/CustomerController.java
+++ src/main/java/com/CodeVictims/App/controllers/CustomerController.java
@@ -1,4 +1,16 @@
 package com.CodeVictims.App.controllers;
 
+import com.CodeVictims.App.services.CustomerServices;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customers")
 public class CustomerController {
+
+    private final CustomerServices customerServices;
+
+    public CustomerController(CustomerServices customerServices){
+        this.customerServices = customerServices;
+    }
 }
