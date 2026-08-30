cc12104e0da5796aaa3ed7e15faaf3410419a733
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-30 14:30:56 +0400

    feat (services): CustomerServices starting writing services

--- src/main/java/com/CodeVictims/App/services/CustomerServices.java
+++ src/main/java/com/CodeVictims/App/services/CustomerServices.java
@@ -1,4 +1,20 @@
 package com.CodeVictims.App.services;
 
+import com.CodeVictims.App.entities.Customer;
+import com.CodeVictims.App.requestobjects.CustomerRequest;
+import com.CodeVictims.App.responseobjects.CustomerResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class CustomerServices {
+
+    private static final List<Customer> customerList = new ArrayList<>();
+
+    public CustomerResponse createCustomer(CustomerRequest request) {
+
+        if (!verifyCustomerNameAndEmail(request.getCustomerFirstName()))
+    }
 }
