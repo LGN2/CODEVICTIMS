5d39c63e4454a5a70818a293007a678d6f9a7fa9
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-30 12:13:48 +0400

    feat(responseobjects): CustomerResponse added attributes and getter, setter

--- src/main/java/com/CodeVictims/App/responseobjects/CustomerResponse.java
+++ src/main/java/com/CodeVictims/App/responseobjects/CustomerResponse.java
@@ -1,4 +1,13 @@
 package com.CodeVictims.App.responseobjects;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CustomerResponse {
+    String customerId;
+    String fullName;
+    String phoneNumber;
+    String email;
 }
